8283e28a4c2d5bac5dde67874873c0826a8006f8
luciano771 <79546157+luciano771@users.noreply.github.com>
2023-12-13 23:31:52 -0300

    este es el challenge de tacticasoft que hice previamente en winforms con vb.net, pero ahora voy a hacerlo en asp net web api y voy a consumir el front con angular, uso codefirs y algunos patrones de diseño como el repository

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
+using DB;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Inicio Conexion a la base
+builder.Services.AddDbContext<VentasContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("API"));
+    options.LogTo(Console.WriteLine);
+});
+//Fin Conexion a la base
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+ 
+ 
+
 var app = builder.Build();
+ 
+
+//crear scope para que se ejecute cada vez que se ejecuta la app
+using (var scope = app.Services.CreateScope()) 
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<DB.VentasContext>();
+    dataContext.Database.Migrate();
+}
+//fin scope
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
