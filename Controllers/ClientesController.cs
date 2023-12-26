a55211561d4ee563e8b6790fe56b955fb2ae69de
luciano771 <79546157+luciano771@users.noreply.github.com>
2023-12-26 00:55:59 -0300

    separe el proyecto en 3 capas

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataAccess;
+using BussinesLogic;
+using BussinesLogic.Repositories.Interfaces;
+using DataAccess.Entities;
+
+namespace Inventario.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : Controller
+    {
+        private readonly IClientesServices _clientesServices;
+
+        public ClientesController(IClientesServices clientesServices) 
+        {
+            _clientesServices = clientesServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllClientesAsync()
+        {
+            var clientes = await _clientesServices.GetAllClientesAsync();
+            return Ok(clientes.ToList());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,29 @@
-using DB;
+using BussinesLogic.Repositories.Implementations;
+using BussinesLogic.Repositories.Interfaces;
+using DataAccess.Data;
+using DataAccess.Repositories.Implementations;
+using DataAccess.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
+// Registrando la interfaz y su implementación concreta
+ 
+
 
 // Inicio Conexion a la base
-builder.Services.AddDbContext<VentasContext>(options =>
+builder.Services.AddDbContext<IventarioContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("API"));
     options.LogTo(Console.WriteLine);
 });
 //Fin Conexion a la base
+
+
+builder.Services.AddScoped<IClientesRepository, ClientesRepository>();
+builder.Services.AddScoped<IClientesServices, ClientesServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,19 +38,18 @@ var app = builder.Build();
  
 
 //crear scope para que se ejecute cada vez que se ejecuta la app
-using (var scope = app.Services.CreateScope()) 
-{
-    var dataContext = scope.ServiceProvider.GetRequiredService<DB.VentasContext>();
-    dataContext.Database.Migrate();
-}
+//using (var scope = app.Services.CreateScope()) 
+//{
+//    var dataContext = scope.ServiceProvider.GetRequiredService<IventarioContext>();
+//    dataContext.Database.Migrate();
+//}
 //fin scope
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+ 
+app.UseSwagger();
+app.UseSwaggerUI();
+ 
 
 app.UseHttpsRedirection();
 
